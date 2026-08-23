ce7f93138fc3f336b665fbe4adf444290cc1752e
Gbenga <gbengaadeboye99@gmail.com>
2026-08-23 10:24:34 +0100

    Update Vercel function configuration

--- api/index.js
+++ api/index.js
@@ -1,12 +1,8 @@
-// import serverless from "serverless-http";
-// import { app, initializeData } from "../server.js";
+import serverless from "serverless-http";
+import { app } from "../server.js";
 
-// const handler = serverless(app);
+const handler = serverless(app);
 
-// export default async function vercelHandler(req, res) {
-//   await initializeData();
-//   return handler(req, res);
-// }
-import app from "../server.js";
-
-export default app;
+export default function vercelHandler(req, res) {
+  return handler(req, res);
+}
