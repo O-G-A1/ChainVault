e58878d7cb9f25ea14e5965dc13904e1eec66384
Gbenga <gbengaadeboye99@gmail.com>
2026-08-23 10:08:05 +0100

    test

--- api/index.js
+++ api/index.js
@@ -1,9 +1,17 @@
+// import serverless from "serverless-http";
+// import { app, initializeData } from "../server.js";
+
+// const handler = serverless(app);
+
+// export default async function vercelHandler(req, res) {
+//   await initializeData();
+//   return handler(req, res);
+// }
 import serverless from "serverless-http";
-import { app, initializeData } from "../server.js";
+import { app } from "../server.js";
 
 const handler = serverless(app);
 
-export default async function vercelHandler(req, res) {
-  await initializeData();
+export default function vercelHandler(req, res) {
   return handler(req, res);
 }
